1b76330b69efc786cee634d03a0866a2a01fffec
FatimaZahraAachak <fatiaachak@gmail.com>
2026-03-08 12:11:51 +0100

    feat: add endpoint to retrieve all users from the database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { db } from "./db/index";
+import { users } from "./db/schema";
 const app = express();
 app.get("/", (req, res) => {
     res.send("Bienvenu sur Mon Gateau")
 })
+app.get("/users", (req, res) => {
+    const allUsers = db.select().from(users).all();
+    res.json(allUsers);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur le port ${PORT}`);
